77311065cbb2bc6e92fbda45362750b110faacf6
VictorAnthony506 <victor0anthony@gmail.com>
2022-07-28 19:57:56 +0100

    third commit

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import render
-from watchlist_app.models import Movie
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from watchlist_app.models import Movie
+# from django.http import JsonResponse
 
-# Create your views here.
-def movie_list(request):
-    movies = Movie.objects.all()
-    data = list(movies.values())
+# # Create your views here.
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = list(movies.values())
     
-    return JsonResponse({'movie': data})
+#     return JsonResponse({'movie': data})
     
-def movie_details(request, pk):
-    """Function to extract individual information from result of movie_list function"""
-    movie = Movie.objects.get(pk=pk)
-    data = {
-        'name': movie.name,
-        'description': movie.description,
-        'active': movie.active
-    }
-    return JsonResponse(data)
+# def movie_details(request, pk):
+#     """Function to extract individual information from result of movie_list function"""
+#     movie = Movie.objects.get(pk=pk)
+#     data = {
+#         'name': movie.name,
+#         'description': movie.description,
+#         'active': movie.active
+#     }
+#     return JsonResponse(data)
     
