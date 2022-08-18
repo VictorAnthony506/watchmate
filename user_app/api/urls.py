f13b5cf58febdc1e54cec174f56316c7ee6f8188
VictorAnthony506 <victor0anthony@gmail.com>
2022-08-18 23:41:24 +0100

    Aug 7

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,12 +1,13 @@
 from importlib.resources import path
 from django.contrib import admin
 from django.urls import path
-from user_app.api.views import registration_view
+from user_app.api.views import registration_view, logout_view
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', registration_view, name='register'),
+    path('logout/', logout_view, name='logout'),
     
 ]
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,7 +1,19 @@
+from tokenize import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 from user_app.api.serializers import RegistrationSerializer
+from user_app import models
 
+
+@api_view(['POST',])
+def logout_view(request):
+    
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+    
 
 @api_view(['POST', ])
 def registration_view(request):
@@ -9,8 +21,21 @@ def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
         
+        data = {}
+        
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+            
+            data['response'] = "Registration Successfull!"
+            data['username'] = account.username
+            data['email'] = account.email
+            
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+            
+        else:
+            data = serializer.errors    
+        
+        return Response(data)
         
     
